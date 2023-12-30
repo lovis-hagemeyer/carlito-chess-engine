98e1793dfc98d76822d40da085e739a815a5aaa8
L. Hagemeyer <l_o_v_i_s@yahoo.de>
2023-12-30 22:39:46 +0100

    removed build outputs

--- target/debug/deps/carlito-996e36728f276801.d
+++
@@ -1,9 +0,0 @@
-/home/lovis/projects/carlito/target/debug/deps/libcarlito-996e36728f276801.rmeta: src/main.rs src/bitboard.rs src/position.rs src/chess_move.rs src/uci.rs
-
-/home/lovis/projects/carlito/target/debug/deps/carlito-996e36728f276801.d: src/main.rs src/bitboard.rs src/position.rs src/chess_move.rs src/uci.rs
-
-src/main.rs:
-src/bitboard.rs:
-src/position.rs:
-src/chess_move.rs:
-src/uci.rs:
--- target/debug/deps/carlito-c16140b31c917d21.d
+++
@@ -1,9 +0,0 @@
-/home/lovis/projects/carlito/target/debug/deps/carlito-c16140b31c917d21: src/main.rs src/bitboard.rs src/position.rs src/chess_move.rs src/uci.rs
-
-/home/lovis/projects/carlito/target/debug/deps/carlito-c16140b31c917d21.d: src/main.rs src/bitboard.rs src/position.rs src/chess_move.rs src/uci.rs
-
-src/main.rs:
-src/bitboard.rs:
-src/position.rs:
-src/chess_move.rs:
-src/uci.rs:
